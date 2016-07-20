0ec1baaa6567346a541315b6b878acf2f5b2def5
Nastya <xatokya@gmail.com>
2016-07-20 23:05:31 +0300

    Rectangle added

---
+++ Shape/src/Rectangle.java
@@ -0,0 +1,30 @@
+
+public class Rectangle implements  Shape {
+    double height;
+    double width;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPerimeter() {
+        return 2*height+2*width;
+    }
+}
